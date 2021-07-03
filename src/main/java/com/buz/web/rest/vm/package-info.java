4e130eb8052efe7372896df5fc7c2ebf6274280b
jeromemst <jerome.r@mstsolutions.com>
2021-07-03 20:55:45 +0530

    Initial version of reelbuuz generated by generator-jhipster@7.1.0

---
+++ src/main/java/com/buz/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.buz.web.rest.vm;
---
+++ src/test/java/com/buz/ArchTest.java
@@ -0,0 +1,29 @@
+package com.buz;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.core.importer.ImportOption;
+import org.junit.jupiter.api.Test;
+
+class ArchTest {
+
+    @Test
+    void servicesAndRepositoriesShouldNotDependOnWebLayer() {
+        JavaClasses importedClasses = new ClassFileImporter()
+            .withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_TESTS)
+            .importPackages("com.buz");
+
+        noClasses()
+            .that()
+            .resideInAnyPackage("com.buz.service..")
+            .or()
+            .resideInAnyPackage("com.buz.repository..")
+            .should()
+            .dependOnClassesThat()
+            .resideInAnyPackage("..com.buz.web..")
+            .because("Services and repositories should not depend on web layer")
+            .check(importedClasses);
+    }
+}
